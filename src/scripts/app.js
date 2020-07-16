fd18b7bed2f4896507f73c034d3d5576824b1bc9
Lucien Mary <marylucien1998@gmail.com>
2020-07-16 16:56:27 +0200

    Test effet glitch sur H1 + bruit en fond + pointeur sur H1

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,3 +3,25 @@ import { name } from './components/hello2';
 
 console.info(`Hello ${hello.name}`);
 console.info(`Hello ${name()}`);
+
+
+// Noise.
+$(window).on('load', addNoise);
+
+function addNoise() {
+  $('.noise-wrapper').css('opacity',0.25);
+}
+
+// cursor glitch.
+function glitchCursor() {
+    $('.glitch-cursor-select').toggleClass('glitch-cursor glitch-cursor--second');
+}
+
+(function loop() {
+    var rand = Math.round(Math.random() * (2000 - 800)) + 0;
+    setTimeout(function() {
+            //alert('A');
+            glitchCursor();
+            loop();
+    }, rand);
+}());
